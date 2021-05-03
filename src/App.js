b927839888a201dc6e7722878b16abfed5c5040b
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-05-03 13:47:37 +0200

    Install React router, create skeleton

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from './Pages/Home.js';
+import Game from './Pages/Game.js';
+import Leaderboard from './Pages/Leaderboard.js';
+import MissingPage from './Pages/MissingPage.js';
+
 function App() {
-  return <div className="App">I'm working</div>;
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/game" exact render={Game} />
+        <Route path="/leaderboard" exact render={Leaderboard} />
+        <Route path="/*" component={MissingPage} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/Pages/MissingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MissingPage(props) {
+  return <div>Missing Page</div>;
+}
+
+export default MissingPage;
