1a06ef568289bbe7e35e9b3bf365ebbd9b29ccfe
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-05-04 14:22:26 +0200

    Show target box in the correct position

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -21,22 +21,26 @@ let GameImage = styled.img.attrs((props) => ({
 
 let TargetBox = styled.div`
   position: absolute;
-  top: ${(props) => props.pos.y}px;
-  left: ${(props) => props.pos.x}px;
-  border: 2px solid white;
+  top: ${(props) => props.pos.y - 40}px;
+  left: ${(props) => props.pos.x - 40}px;
+  border: 3px solid white;
   background: transparent;
-  height: 50px;
-  width: 50px;
+  height: 80px;
+  width: 80px;
 `;
 
 function Home(props) {
   let [showTarget, setShowTarget] = useState(false);
+  let [target, setTarget] = useState({ x: 0, y: 0 });
   let [targetPos, setTargetPos] = useState({ x: 0, y: 0 });
 
   let handleClick = (e) => {
-    let { offsetX, offsetY } = e.nativeEvent;
+    let { offsetX, offsetY, pageX, pageY } = e.nativeEvent;
     setShowTarget(true);
-    setTargetPos({ x: offsetX, y: offsetY });
+    setTargetPos({ x: pageX, y: pageY });
+    setTarget({ x: offsetX, y: offsetY });
+
+    console.log(offsetX, offsetY);
   };
 
   return (
