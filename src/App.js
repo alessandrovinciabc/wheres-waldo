b89d4a90868433029b1bf75511a418ecb3657123
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-05-04 10:12:21 +0200

    Remove Game page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './Pages/Home.js';
-import Game from './Pages/Game.js';
 import Leaderboard from './Pages/Leaderboard.js';
 import MissingPage from './Pages/MissingPage.js';
 
@@ -16,7 +15,6 @@ function App() {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/game" exact render={Game} />
           <Route path="/leaderboard" exact render={Leaderboard} />
           <Route path="/*" component={MissingPage} />
         </Switch>
